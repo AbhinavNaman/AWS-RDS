c5048fb2aa6ea61b15e9eba1d9f689116ee47f8b
abhinav naman <abhinavnaman3@gmail.com>
2025-03-26 10:20:09 +0530

    test

--- data.tf
+++ data.tf
@@ -1,8 +1,8 @@
-data "aws_security_group" "rds-sg" {
-  name = "rds-sg"
-}
+# data "aws_security_group" "rds-sg" {
+#   name = "rds-sg"
+# }
 
-data "aws_key_pair" "example" {
-  key_name = "terraform-rds"
-}
+# data "aws_key_pair" "example" {
+#   key_name = "terraform-rds"
+# }
 
